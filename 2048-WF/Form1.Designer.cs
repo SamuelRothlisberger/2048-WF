88e030ebc993829ff6628152a9774ff0c3fd32c1
SamuelRothlisberger <150002204+SamuelRothlisberger@users.noreply.github.com>
2024-01-08 11:29:24 +0100

    Transfer from Console to Windows Form

--- 2048-WF/Form1.Designer.cs
+++ 2048-WF/Form1.Designer.cs
@@ -28,76 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.board = new System.Windows.Forms.DataGridView();
-            this.col1 = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.col3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.col4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.board)).BeginInit();
             this.SuspendLayout();
-            // 
-            // board
-            // 
-            this.board.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
-            this.board.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.board.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.col1,
-            this.col2,
-            this.col3,
-            this.col4});
-            this.board.GridColor = System.Drawing.SystemColors.Control;
-            this.board.Location = new System.Drawing.Point(75, 116);
-            this.board.Name = "board";
-            this.board.Size = new System.Drawing.Size(300, 300);
-            this.board.TabIndex = 0;
-            this.board.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.board_CellContentClick);
-            // 
-            // col1
-            // 
-            this.col1.HeaderText = "col1";
-            this.col1.Name = "col1";
-            this.col1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.col1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            this.col1.Width = 75;
-            // 
-            // col2
-            // 
-            this.col2.HeaderText = "col2";
-            this.col2.Name = "col2";
-            this.col2.Width = 75;
-            // 
-            // col3
-            // 
-            this.col3.HeaderText = "col3";
-            this.col3.Name = "col3";
-            this.col3.Width = 75;
-            // 
-            // col4
-            // 
-            this.col4.HeaderText = "col4";
-            this.col4.Name = "col4";
-            this.col4.Width = 75;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(452, 495);
-            this.Controls.Add(this.board);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.board)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView board;
-        private System.Windows.Forms.DataGridViewComboBoxColumn col1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn col2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn col3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn col4;
     }
 }
 
--- 2048-WF/Form1.cs
+++ 2048-WF/Form1.cs
@@ -13,68 +13,89 @@ namespace _2048_WF
     public partial class Form1 : Form
     {
         static int score = 0;
+        static Random random = new Random();
+        static int[,] board = new int[4, 4];
+        static bool win = false;
+
+        Label[,] lbl = new Label[4, 4]; //tableau de Labels 4 lignes et 4 colonnes
 
         public Form1()
         {
             InitializeComponent();
+            InitializeJeu();
+            KeyDown += Form1_KeyDown;
         }
 
-        private void board_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void InitializeJeu()
         {
-            // Titre de la fenêtre
-            Console.Title = "2048 Game";
+            // Création des labels
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 4; col++)
+                {
+                    lbl[row, col] = new Label(); //Création du label
 
-            // Créer un tableau à 2 dimensions
-            int[,] board = new int[4, 4];
+                    // le 20 + 100 * colonne détermine où placer le premier label dans le form en X
+                    // le 20 + 100 * ligne détermine où placer le label en Y
+                    // le 90, 90 est la taille du label
+                    lbl[row, col].Bounds = new Rectangle(20 + 100 * col, 20 + 100 * row, 90, 90);
+
+                    // met le texte au milieu du label
+                    lbl[row, col].TextAlign = ContentAlignment.MiddleCenter;
+                    lbl[row, col].Font = new Font("Arial", 20);
+
+                    Controls.Add(lbl[row, col]); //Ajout visible à la page
+                }
+            }
 
             //Initialise le tableau avec 2 chiffres aléatoires
             for (int start = 0; start < 2; start++)
             {
-                //AddNewNumber(board);
+                AddNewNumber();
+                DisplayBoard();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Sauvegarde le tableau actuel dans une variable
+            int[,] previousBoard = (int[,])board.Clone();
+
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    MoveRight();
+                    break;
+
+                case Keys.Left:
+                    MoveLeft();
+                    break;
+
+                case Keys.Up:
+                    MoveUp();
+                    break;
+
+                case Keys.Down:
+                    MoveDown();
+                    break;
+
+                default:
+                    MessageBox.Show("Tu dois appuyer sur les flèches directionnelles");
+                    break;
+            }
+            DisplayBoard();
+
+            // Verifie si le tableau a changé
+            if (!BoardEqual(previousBoard, board))
+            {
+                AddNewNumber(); // Ajoute un nombre si le tableau a changé
+                DisplayBoard();
             }
 
-            // Boucle principale du jeu
-            while (true)
+            if (Lose() == true)
             {
-                // Afficher la grille 4x4
-                DisplayBoard(board);
-
-                // Obtenir la touche pressée
-                ConsoleKeyInfo keyInfo = Console.ReadKey();
-                ConsoleKey key = keyInfo.Key;
-
-                // Sauvegarde le tableau actuel dans une variable
-                int[,] previousBoard = (int[,])board.Clone();
-
-                switch (key)
-                {
-                    case ConsoleKey.RightArrow:
-                        //MoveRight(board);
-                        break;
-
-                    case ConsoleKey.LeftArrow:
-                        //MoveLeft(board);
-                        break;
-
-                    case ConsoleKey.UpArrow:
-                        //MoveUp(board);
-                        break;
-
-                    case ConsoleKey.DownArrow:
-                        //MoveDown(board);
-                        break;
-
-                    default:
-                        Console.WriteLine("Tu dois appuyer sur les flèches directionnelles");
-                        break;
-                }
-                // Verifie si le tableau a changé
-                if (!BoardEqual(previousBoard, board))
-                {
-                    //AddNewNumber(board); // Ajoute un nombre si le tableau a changé
-                }
-                //Lose(board);
-                //Win(board);
+                MessageBox.Show("Tu as perdu !\n\nScore : " + score.ToString());
+                this.Close();
             }
         }
 
@@ -94,44 +115,272 @@ namespace _2048_WF
             return true;
         }
 
-
-        // Fonction pour afficher la grille 4x4
-        static void DisplayBoard(int[,] board)
+        private void DisplayBoard()
         {
-            Console.Clear();
-
-            Console.Write("-------------------------------");
-            Console.Write("\n---------- 2048 Game ----------");
-            Console.Write("\n-------------------------------\n\n\n");
-
+            //réaffiche tout le tableau avec les bonnes couleurs et les bons textes, conformément au tableau jeu
             for (int row = 0; row < 4; row++)
             {
                 for (int col = 0; col < 4; col++)
-
-                Console.WriteLine("\n\n");
+                {
+                    if (board[row, col] > 0)
+                        lbl[row, col].Text = (board[row, col]).ToString();
+                    else
+                        lbl[row, col].Text = "";
+                    lbl[row, col].BackColor = GetColor(board[row, col]);
+                }
             }
-            Console.WriteLine("\nScore : " + score);
+        }
+
+        // Fonction pour déplacer les chiffres vers la droite
+        static void MoveRight()
+        {
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 2; col >= 0; col--)
+                {
+                    if (board[row, col] != 0)
+                    {
+                        int currentCol = col;
+
+                        //Déplacement vers la droite et fusion des nombres
+                        while (currentCol + 1 < 4 && (board[row, currentCol + 1] == 0 || board[row, currentCol + 1] == board[row, currentCol]))
+                        {
+                            if (board[row, currentCol + 1] == 0)
+                            {
+                                // Déplacement vers la droite
+                                board[row, currentCol + 1] = board[row, currentCol];
+                                board[row, currentCol] = 0;
+                                currentCol++;
+                            }
+                            else if (board[row, currentCol + 1] == board[row, currentCol])
+                            {
+                                // Fusion des nombres de même valeur
+                                board[row, currentCol + 1] *= 2;
+                                board[row, currentCol] = 0;
+
+                                // Ajouter la valeur fusionnée au score
+                                score += board[row, currentCol + 1];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Fonction pour déplacer les chiffres vers la gauche
+        static void MoveLeft()
+        {
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 1; col < 4; col++)
+                {
+                    if (board[row, col] != 0)
+                    {
+                        int currentCol = col;
+
+                        //Déplacement vers la gauche et fusion des nombres
+                        while (currentCol - 1 >= 0 && (board[row, currentCol - 1] == 0 || board[row, currentCol - 1] == board[row, currentCol]))
+                        {
+                            // Déplacement vers la gauche
+                            if (board[row, currentCol - 1] == 0)
+                            {
+                                board[row, currentCol - 1] = board[row, currentCol];
+                                board[row, currentCol] = 0;
+                                currentCol--;
+                            }
+                            else if (board[row, currentCol - 1] == board[row, currentCol])
+                            {
+                                // Fusion des nombres de même valeur
+                                board[row, currentCol - 1] *= 2;
+                                board[row, currentCol] = 0;
+
+                                // Ajouter la valeur fusionnée au score
+                                score += board[row, currentCol - 1];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Fonction pour déplacer les chiffres vers le haut
+        static void MoveUp()
+        {
+            for (int col = 0; col < 4; col++)
+            {
+                for (int row = 1; row < 4; row++)
+                {
+                    if (board[row, col] != 0)
+                    {
+                        int currentRow = row;
+
+                        //Déplacement vers le haut et fusion des nombres
+                        while (currentRow - 1 >= 0 && (board[currentRow - 1, col] == 0 || board[currentRow - 1, col] == board[currentRow, col]))
+                        {
+                            // Déplacement vers le haut
+                            if (board[currentRow - 1, col] == 0)
+                            {
+                                board[currentRow - 1, col] = board[currentRow, col];
+                                board[currentRow, col] = 0;
+                                currentRow--;
+                            }
+                            else if (board[currentRow - 1, col] == board[currentRow, col])
+                            {
+                                // Fusion des nombres de même valeur
+                                board[currentRow - 1, col] *= 2;
+                                board[currentRow, col] = 0;
+
+                                // Ajouter la valeur fusionnée au score
+                                score += board[currentRow - 1, col];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Fonction pour déplacer les chiffres vers le bas
+        static void MoveDown()
+        {
+            for (int col = 0; col < 4; col++)
+            {
+                for (int row = 2; row >= 0; row--)
+                {
+                    if (board[row, col] != 0)
+                    {
+                        int currentRow = row;
+
+                        //Déplacement vers le bas et fusion des nombres
+                        while (currentRow + 1 < 4 && (board[currentRow + 1, col] == 0 || board[currentRow + 1, col] == board[currentRow, col]))
+                        {
+                            // Déplacement vers le bas
+                            if (board[currentRow + 1, col] == 0)
+                            {
+                                board[currentRow + 1, col] = board[currentRow, col];
+                                board[currentRow, col] = 0;
+                                currentRow++;
+                            }
+                            else if (board[currentRow + 1, col] == board[currentRow, col])
+                            {
+                                // Fusion des nombres de même valeur
+                                board[currentRow + 1, col] *= 2;
+                                board[currentRow, col] = 0;
+
+                                // Ajouter la valeur fusionnée au score
+                                score += board[currentRow + 1, col];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Fonction pour ajouter un nouveau '2' dans le tableau à chaque mouvement
+        static void AddNewNumber()
+        {
+            // Obtient la valeur du nouveau chiffre avec 90% de chance d'avoir un '2' et 10% de chance d'avoir un '4'
+            int value;
+
+            if (random.Next(10) < 9)
+            {
+                value = 2;  // 90% de chance d'avoir un '2'
+            }
+            else
+            {
+                value = 2048;  // 10% de chance d'avoir un '4'
+            }
+
+            // Recherche d'une position vide pour ajouter un nouveau chiffre
+            int row, col;
+
+            do
+            {
+                row = random.Next(0, 4);
+                col = random.Next(0, 4);
+            }
+            while (board[row, col] != 0);
+
+            // Ajout du nouveau chiffre
+            board[row, col] = value;
         }
 
         // Fonction pour avoir la couleur en fonction de la valeur de la case
-        static ConsoleColor GetColor(int value)
+        static Color GetColor(int value)
         {
             switch (value)
             {
-                case 0: return ConsoleColor.White;
-                case 2: return ConsoleColor.White;
-                case 4: return ConsoleColor.Gray;
-                case 8: return ConsoleColor.DarkGray;
-                case 16: return ConsoleColor.Yellow;
-                case 32: return ConsoleColor.DarkYellow;
-                case 64: return ConsoleColor.Red;
-                case 128: return ConsoleColor.DarkRed;
-                case 256: return ConsoleColor.Magenta;
-                case 512: return ConsoleColor.DarkMagenta;
-                case 1024: return ConsoleColor.DarkBlue;
-                case 2048: return ConsoleColor.Blue;
-                default: return ConsoleColor.Blue;
+                case 0: return Color.White;
+                case 2: return Color.LightGray;
+                case 4: return Color.LightGreen;
+                case 8: return Color.MistyRose;
+                case 16: return Color.Yellow;
+                case 32: return Color.SandyBrown;
+                case 64: return Color.Orange;
+                case 128: return Color.Tomato;
+                case 256: return Color.Magenta;
+                case 512: return Color.DarkMagenta;
+                case 1024: return Color.DarkBlue;
+                case 2048: return Color.Blue;
+                default: return Color.Blue;
             }
+        }
+
+        //Fonction qui vérifie si le joueur a perdu ou non
+        static bool Lose()
+        {
+            //Vérifie si une case est vide
+            foreach (var number in board)
+            {
+                if (number == 0)
+                {
+                    return false;
+                }
+            }
+
+            // Vérifie si des mouvements sont encore possible horizontalement
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    if (board[row, col] == board[row, col + 1] || board[row, col] == 0 || board[row, col + 1] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // Vérifie si des mouvements sont encore possible verticalement
+            for (int col = 0; col < 4; col++)
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    if (board[row, col] == board[row + 1, col] || board[row, col] == 0 || board[row + 1, col] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            // Si aucun mouvement possible, le joueur a perdu
+            return true;
+        }
+
+        // Fonction qui vérifie si le joueur a gagné
+        static bool Win()
+        {
+            // Cherche dans le tableau si une case 2048 ou plus est présente
+            foreach (var number in board)
+            {
+                if (number == 2048 && win == false)
+                {
+                    win = true; // Si une case 2048 ou plus est présente retourne vrai
+                    MessageBox.Show("Bravo tu as gagné ! Tu peux continuer a jouer");
+                }
+            }
+            return false; // Si aucune case 2048 ou plus est présente retourne faux
         }
     }
 }
